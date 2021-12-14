89f8c4653cbb4a5ee0352d654a65d0c42e0de6e4
Tony <awalker780@gmail.com>
2021-12-13 20:59:19 -0500

    Implements renderTweets() function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,27 +4,41 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// Driver code
 
 
 $(document).ready(function() {
   console.log("Test script running!!!");
+
+  // Driver code
   
-  const tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
+  const tweetData = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
         "handle": "@SirIsaac"
       },
-    "content": {
+      "content": {
         "text": "If I have seen further it is by standing on the shoulders of giants"
       },
-    "created_at": 1461116232227
-  }
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
 
   function createTweetElement(tweetData) {
 
-    const tweet = 
+    let $tweet = 
     `<br>
     <section>
       <article class="tweet-display">
@@ -62,12 +76,15 @@ $(document).ready(function() {
       </article>
     </section>`
 
-    return tweet;
+    return $tweet;
   };
 
-  const $tweet = createTweetElement(tweetData);
+  function renderTweets(data) {
+    for (let obj of data) {
+      $(".container").append(createTweetElement(obj));
+    }
+  };
 
-  console.log($tweet);
-  $(".container").append($tweet);
+  renderTweets(tweetData);
 
 });
