a19337d6f9ed043b23116948bbec5d9779231e98
Tony <awalker780@gmail.com>
2021-12-15 18:58:09 -0500

    Implements ajax get request in client.js and removes driver code

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,35 +4,7 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-
-
 $(document).ready(function() {
-  // Driver code
-  
-  const tweetData = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
 
   function createTweetElement(tweetData) {
 
@@ -83,6 +55,16 @@ $(document).ready(function() {
     }
   };
 
-  renderTweets(tweetData);
+  function loadTweets() {
+    $.ajax({
+      type: "GET",
+      url: "http://localhost:8080/tweets",
+    }).then(response => {
+      renderTweets(response);
+    })
+
+  };
+
+  loadTweets();
 
 });
