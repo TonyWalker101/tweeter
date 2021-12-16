9d2e54d99d8debf142c0f9cea65fa968f46d2ee3
Tony <awalker780@gmail.com>
2021-12-15 22:11:44 -0500

    Implements form validation for tweet box

--- public/scripts/form-submission.js
+++ public/scripts/form-submission.js
@@ -2,21 +2,25 @@ $(document).ready(function() {
 
   $(".tweet-form").submit(function(event) {
     event.preventDefault();
+    
+    const textLength = $(this).serializeArray()["0"].value.length;
 
-    console.log("Form submitted...performing ajax call");
-
-    $.ajax({
-      type: "POST",
-      url: "http://localhost:8080/tweets",
-      data: $(this).serialize()
-    }).then(response => {
-      console.log("Successfully posted to the server using Ajax.");
-    });
+    if (textLength === 0) {
+      alert("Please type your Tweet in order to submit!");
+    } else if (textLength > 140) {
+      alert("Please reduce the number of characters in your Tweet in order to submit! (Max: 140)");
+    } else {
+      console.log("Form submitted...performing ajax call");
+  
+      $.ajax({
+        type: "POST",
+        url: "http://localhost:8080/tweets",
+        data: $(this).serialize()
+      }).then(response => {
+        console.log("Successfully posted to the server using Ajax.");
+      });
+    }
 
   });
-
-
   
-
-
 });
